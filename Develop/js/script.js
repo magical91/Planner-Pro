419bd601142037c5596550f7d542315ee837290c
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-13 10:55:24 -0500

    added present date

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,7 +1,5 @@
 // set present day and date
 
-// set up daily planner structure for standard business hours
-
 // make time slots clickable for task input and editing
 
 // make save button and add save to localStorage
@@ -9,4 +7,4 @@
 // color code status of time status event to represent past, present, or future
 
 var rightNow = moment().format('dddd, MMMM Do YYYY');
-
+document.getElementById("currentDay").innerText = rightNow;
