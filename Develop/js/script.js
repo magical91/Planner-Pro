f5e6f8eef3b825046317c98d8cb886880755c52a
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-13 10:27:22 -0500

    added HTML elements to create structure of daily planner

---
+++ Develop/js/script.js
@@ -0,0 +1,12 @@
+// set present day and date
+
+// set up daily planner structure for standard business hours
+
+// make time slots clickable for task input and editing
+
+// make save button and add save to localStorage
+
+// color code status of time status event to represent past, present, or future
+
+var rightNow = moment().format('dddd, MMMM Do YYYY');
+
